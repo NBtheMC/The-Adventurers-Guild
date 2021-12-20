ad2c24eb975fb27bd0dc577077eeac752d280e9b
Eric Long <ericlong292@gmail.com>
2021-12-19 17:39:39 -0800

    Added basic logic data structures
    They are the character_sheet, party_sheet, quest, and event nodes. They're not all functioning, but they are there.

--- Assets/Eric's testing Area/character_sheet.cs
+++ Assets/Eric's testing Area/character_sheet.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class character_sheet : MonoBehaviour
+public class Character_sheet
 {
-    // Start is called before the first frame update
-    void Start()
+    // The character's name
+    public string name { get; private set; }
+
+    // The character's stats
+    private Dictionary<string, int> statlines = new Dictionary<string, int>();
+
+    public Character_sheet(string name, int exploration, int diplomacy, int combat, int stamina)
     {
-        
+        statlines.Add("exploration", exploration);
+        statlines.Add("diplomacy", diplomacy);
+        statlines.Add("combat", combat);
+        statlines.Add("stamina", stamina);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Getter fuctions for the default 4 stats: exploration, diplomacy, combat, and stamina.
+    public int getExploration() { return statlines["exploration"]; }
+    public int getDiplomacy() { return statlines["diplomacy"]; }
+    public int getCombat() { return statlines["combat"]; }
+    public int getStamina() { return statlines["stamina"]; }
+
+    // Add the following stat into the character's statlines.
+    public void addStat(string statname, int number)
     {
-        
+        statlines.Add(statname, number);
     }
+}
+
+public class Party_sheet
+{
+    public string name;
+    public List<Character_sheet> party_members = new List<Character_sheet>();
+}
+
+public class event_node
+{
+    public string name;
+}
+
+public class Quest_sheet
+{
+    public string name;
 }
