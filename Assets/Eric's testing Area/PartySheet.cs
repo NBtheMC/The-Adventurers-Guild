f309e4f81503a96beb853b06bf43b046623626ec
Eric Long <ericlong292@gmail.com>
2021-12-25 17:19:20 -0800

    Defined Quest and Event classes
    They're not functioning in any way, but they are somewhat more defined, and we're just trying to figure out how they function.

---
+++ Assets/Eric's testing Area/PartySheet.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PartySheet
+{
+    public string name;
+    public List<CharacterSheet> party_members = new List<CharacterSheet>();
+
+    public void addMember(CharacterSheet adventurer)
+    {
+        party_members.Add(adventurer);
+    }
+
+    /// <summary>
+    /// Allows the removal of any adventurer that already in the party.
+    /// </summary>
+    /// <param name="adventurer">The charactersheet of the adventurer</param>
+    /// <returns>
+    /// A boolean depending on whether the adventurer was successfully removed.
+    /// </returns>
+    public bool removeMember(CharacterSheet adventurer)
+    {
+        if (!party_members.Contains(adventurer)) { return false; }
+        party_members.Remove(adventurer);
+        return true;
+    }
+
+    /// <summary>
+    /// Returns to sum of the stats for each party member.
+    /// </summary>
+    /// <param name="stat">A string of what stat it is.</param>
+    /// <returns>The Summed stat.</returns>
+    public int getStatSummed(string stat)
+    {
+        int statTotal = 0;
+        foreach (CharacterSheet adventurer in party_members)
+        {
+            statTotal += adventurer.getStat(stat);
+        }
+        return statTotal;
+    }
+}
---
+++ Assets/Eric's testing Area/QuestSheet.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestSheet
+{
+	private EventNode headConnection;
+	private PartySheet adventuring_party;
+	public QuestSheet(EventNode connection_input)
+	{
+		headConnection = connection_input;
+	}
+
+	public void assignParty(PartySheet party_input)
+	{
+		adventuring_party = party_input;
+	}
+	
+	// Calculates the maximum total reward for the quest as given.
+	// Done by doing exhaustive recursive search through the tree and calculating all rewards.
+	public int rewardTotal()
+	{
+		int countingTotal = 0;
+
+		return countingTotal;
+	}
+}
+
+public class EventNode
+{
+	public string stat { get; protected set; }
+	public int DC { get; protected set; }
+	protected EventNode connection;
+	public int time { get; protected set; }
+
+	public EventNode()
+	{
+		stat = "";
+		DC = 0;
+		time = 0;
+		connection = null;
+	}
+	public EventNode(string stat_Input, int DC_input, int time_input)
+	{
+		stat = stat_Input;
+		DC = DC_input;
+		time = time_input;
+		connection = null;
+	}
+}
+
+/// <summary>
+/// This is for Event nodes that have two connections, 1 for DC success, 1 for DC failure.
+/// Currently unncessary for the linear quest progressions we're creating.
+/// </summary>
+public class FailsafeEventNode:EventNode {
+	public FailsafeEventNode(string stat_Input, int DC_input, int time_input) : base(stat_Input, DC_input, time_input)
+	{
+	}
+}
