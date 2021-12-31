a9cc793f3f8ed4ebb5142b11b774b990fc3ca24f
parmpreet001 <parmpreet1337@gmail.com>
2021-12-30 17:25:34 -0800

    Squashed commit of the following:
    commit a57a7df0a5987fcb42a1d81dbf0c5d6906f73e95
    Author: Eric Long <ericlong292@gmail.com>
    Date:   Wed Dec 29 22:18:37 2021 -0800
    
        Moved scripts to the scripts folder, made a new scene
    
        Gonna be working on UI now. Will branch to new branch.
    
    commit b17ce120dc2ac754e22c2092e249facd79f57b84
    Author: Eric Long <ericlong292@gmail.com>
    Date:   Wed Dec 29 20:56:46 2021 -0800
    
        ahhh last change I swear.
    
    commit 3b18196ebe88bae2f70fecaf92cbb787c29971fa
    Author: Eric Long <ericlong292@gmail.com>
    Date:   Wed Dec 29 20:55:23 2021 -0800
    
        Updated QuestSheet to Doc Specification
    
        Cause I had to change a few things to be more in line with the written documentation.

--- Assets/Scripts/DataStructures/EventNode.cs
+++ Assets/Scripts/DataStructures/EventNode.cs
@@ -1,79 +1,6 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class QuestSheet
-{
-	public string questName { get; private set; }
-	private EventNode headConnection; // Tells the graph where the head is going to be.
-	private EventNode currentConnection; // Used during the course of execution to update what the current event is.
-	private PartySheet adventuring_party; // Reference to the adventuring party attached to the quest.
-	public QuestingManager questingManager; // reference to the master questing manager.
-
-	private int eventTicksElapsed; // Tracks how many ticks has elapsed and executes events appropriatly.
-	public bool QuestComplete { get; private set; } // Indicator for QuestingManager to see if the quest is done.
-	public int accumutatedGold { get; private set; } // How much gold has been accumulated from the events.
-
-	/// <summary>
-	/// QuestSheet Constructor
-	/// </summary>
-	/// <param name="connection_input">Head of the event graph</param>
-	/// <param name="name_Input">Name of the Quest</param>
-	public QuestSheet(EventNode connection_input, string name_Input)
-	{
-		headConnection = connection_input;
-		currentConnection = headConnection;
-		eventTicksElapsed = 0;
-		questName = name_Input;
-		QuestComplete = false;
-		accumutatedGold = 0;
-	}
-
-	/// <summary>
-	/// Assigns a part to the quest.
-	/// </summary>
-	/// <param name="party_input">The party to assign to the quest.</param>
-	public void assignParty(PartySheet party_input)
-	{
-		adventuring_party = party_input;
-	}
-
-	// Calculates the maximum total reward for the quest as given.
-	// Done by doing brute-force search through the tree and calculating all possible rewards.
-	public int EstimatedRedwardTotal()
-	{
-		int countingTotal = 0;
-		// Put code here.
-		return countingTotal;
-	}
-
-	/// <summary>
-	/// Calls on the current event to see what's going on.
-	/// </summary>
-	/// <returns>A 0 if the quest is still ongoing. A 1 if the quest is complete.</returns>
-	public int advancebyTick()
-	{
-		if (eventTicksElapsed >= currentConnection.time)
-		{
-			// Reset the event tick timer.
-			eventTicksElapsed = 0;
-
-			// Request the event package.
-			EventNode.EventPackage returnMessage = currentConnection.resolveEvent(adventuring_party);
-
-			// Progress to the next event.
-			if (returnMessage.nextEvent != null)
-			{
-				currentConnection = returnMessage.nextEvent;
-				return advancebyTick();
-			}
-			else { return 1; }
-		}
-		eventTicksElapsed++;
-
-		return 0;
-	}
-}
 
 /// <summary>
 /// The default event node. Inherited by all other event nodes.
@@ -84,7 +11,7 @@ public class QuestSheet
 public class EventNode
 {
 	public string stat; // the stat to be checked against. Should correspond with PartySheet
-	public int DC; 
+	public int DC;
 	protected List<EventNode> connection; // A list of connections to get used.
 	public int time; // How many ticks before the DC check is triggered
 	public EventTypes eventType; // used to decide how EventNode will handle 
@@ -94,9 +21,9 @@ public class EventNode
 	/// </summary>
 	public enum EventTypes
 	{
-		head = 0, // Nodes without preconditions.
-		successful = 1, // Nodes that need succesful DC check
-		fail = 2, // Nodes that need failed DC check
+		head = 1, // Nodes without preconditions.
+		successful = 2, // Nodes that need succesful DC check
+		fail = 3, // Nodes that need failed DC check
 	}
 
 	public class EventPackage
@@ -129,7 +56,7 @@ public class EventNode
 		DC = DCInput;
 		time = timeInput;
 		connection = new List<EventNode>();
-		eventType=EventTypes.head;
+		eventType = EventTypes.head;
 	}
 
 	/// <summary>
@@ -140,8 +67,8 @@ public class EventNode
 	public void addConnection(EventNode connection_input, int index = -1)
 	{
 		// If there is no desired input index.
-		if(index == -1){ connection.Add(connection_input);}
-		else { connection.Insert(index,connection_input); }
+		if (index == -1) { connection.Add(connection_input); }
+		else { connection.Insert(index, connection_input); }
 	}
 
 	public EventPackage resolveEvent(PartySheet adventurers)
@@ -169,7 +96,7 @@ public class EventNode
 		return message;
 	}
 
-
+	/*
 	/// <summary>
 	/// Checks if it's time to execute an event. If it is, executes event.
 	/// </summary>
@@ -210,49 +137,5 @@ public class EventNode
 		// If there are enough connections
 		if (input_DC > DC) { return connection[0]; }
 		else { return connection[1]; }
-	}
+	}*/
 }
-
-
-
-
-
-
-
-
-/*
-/// <summary>
-/// Returns 1 on all timeChecks, no matter what.
-/// </summary>
-public class FailEventNode: EventNode
-{
-	public new int timeCheck(int currentTick, int input_DC, ref EventNode currentNode)
-	{
-		return 1;
-	}
-}
-
-/// <summary>
-/// Returns 2 on all timeChecks, no matter what.
-/// </summary>
-public class SuccessEventNode: EventNode
-{
-	public new int timeCheck(int currentTick, int input_DC, ref EventNode eventNode)
-	{
-		return 2;
-	}
-}
-
-/// <summary>
-/// The most common event node, used for events that must succeed in order to progress.
-/// </summary>
-public class LinearEventNode : EventNode
-{
-	new protected EventNode nextConnection(int input_DC)
-	{
-		// If there are enough connections
-		if (input_DC > DC) { return connection[0]; }
-		else { return new FailEventNode(); }
-	}
-}
-*/
