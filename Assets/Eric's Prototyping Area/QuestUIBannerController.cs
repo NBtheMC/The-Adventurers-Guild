917dff58a25e0aad4d7aed3fa985bf00d49d386b
Eric Long <ericlong292@gmail.com>
2022-01-05 23:13:03 -0800

    Some more UI testing
    Apparently there's a sorting order. I'm not going to worry about it right now, we're just going to make banners and hide them when they're not neccessary.

---
+++ Assets/Eric's Prototyping Area/QuestUIBannerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestUIBannerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<RectTransform>().anchoredPosition += new Vector2((float)-0.01,0);
+    }
+}
