1c3f26fe92cea4ea7ba7563408cb5041252bc600
Eric Long <ericlong292@gmail.com>
2022-01-29 16:52:59 -0800

    Created Storylet display.
    Screaming internally. Really it's just some dev tools.

--- Assets/Eric's Prototyping Area/StoryletPrototyping/TestingScripts/WorldIntChanger.cs
+++ Assets/Eric's Prototyping Area/StoryletPrototyping/TestingScripts/WorldIntChanger.cs
@@ -9,42 +9,50 @@ namespace StoryletTesting {
     public class WorldIntChanger : MonoBehaviour
     {
         public TMP_Text text;
-        public string headerMessage;
-        public string worldStat;
+        public string worldStat; // probably should leave blank.
+        public Slider currentSlider; // drag it in.
+        public TMP_InputField inputField; // also drag it in.
         float value;
-        Slider currentSlider;
 
         public WorldStateManager theWorld;
 
         // Start is called before the first frame update
         void Start()
         {
-            // Set references to the slider.
-            currentSlider = this.GetComponent<Slider>();
-
-            // Set up the world to behave with the slider.
-            theWorld.AddWorldValue(worldStat, currentSlider.value);
-
             // get the current world Value
-            value = theWorld.GetWorldValue(worldStat);
+            value = theWorld.GetWorldInt(worldStat);
 
-            // Set the text to the world value.
-            text.text = $"{headerMessage}: {value}";
-            currentSlider.onValueChanged.AddListener(UpdateWorldValue);
+            // Sets the title to the the string of the stat.
+            text.text = worldStat;
+
+            // Ensures that all input are intergers.
+            inputField.contentType = TMP_InputField.ContentType.IntegerNumber;
+
+            // Sets a listener for an event.
+            inputField.onEndEdit.AddListener(UpdateWorldValueFromInput);
         }
 
-        /// <summary>
-        /// What we use to update the world value.
-        /// </summary>
-        /// <param name="inputChange">IDK, it needs this or something.</param>
-        public void UpdateWorldValue(float inputChange)
+		private void Update()
+		{
+		    
+		}
+
+        public void UpdateWorldValueFromInput(string input)
+		{
+            
+		}
+
+		/// <summary>
+		/// What we use to update the world value.
+		/// </summary>
+		/// <param name="inputChange">IDK, it needs this or something.</param>
+		public void UpdateWorldValue(float inputChange)
         {
             // change the world value.
             theWorld.AddWorldValue(worldStat, currentSlider.value);
 
             // Set the text to the world value.
             value = theWorld.GetWorldValue(worldStat);
-            text.text = $"{headerMessage}: {value}";
         }
 
         public void IncrementValue()
