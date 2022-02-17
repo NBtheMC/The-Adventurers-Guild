a6b6310d9ed9fccb83b5d6522be51642c9b40380
Eric Long <ericlong292@gmail.com>
2022-02-17 00:34:45 -0800

    Added two more quests! Yay!
    More things to play with!

--- Assets/Scripts/DataStructures/QuestSheet.cs
+++ Assets/Scripts/DataStructures/QuestSheet.cs
@@ -9,7 +9,7 @@ public class QuestSheet
 	private EventNode headConnection; // Tells the graph where the head is going to be.
 	private EventNode currentConnection; // Used during the course of execution to update what the current event is.
 	private PartySheet adventuring_party; // Reference to the adventuring party attached to the quest.
-	public int partySize { get; private set; } //how many adventurers can be assigned to a quest
+	public int partySize = 4;
 
 	private int eventTicksElapsed; // Tracks how many ticks has elapsed and executes events appropriatly.
 	public bool QuestComplete { get; private set; } // Indicator for QuestingManager to see if the quest is done.
@@ -28,7 +28,7 @@ public class QuestSheet
 	/// </summary>
 	/// <param name="connection_input">Head of the event graph</param>
 	/// <param name="name_Input">Name of the Quest</param>
-	public QuestSheet(EventNode connection_input, string name_Input, int partySize_input = 4, string inputQuestDescription = "")
+	public QuestSheet(EventNode connection_input, string name_Input, string inputQuestDescription = "")
 	{
 		headConnection = connection_input;
 		currentConnection = headConnection;
@@ -36,7 +36,6 @@ public class QuestSheet
 		questName = name_Input;
 		QuestComplete = false;
 		accumutatedGold = 0;
-		partySize = partySize_input;
 		questDescription = inputQuestDescription;
 
         visitedNodes = new List<EventNode>();
