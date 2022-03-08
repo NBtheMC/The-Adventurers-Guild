267da472429aa7f80a41a154c80fc4f1e37e9c95
Joshua <jaugees@gmail.com>
2022-03-07 20:15:39 -0800

    added node+edge functions to WorldGraph
    i have not tested anything yet

--- Assets/Scripts/DataStructures/WorldGraph.cs
+++ Assets/Scripts/DataStructures/WorldGraph.cs
@@ -4,32 +4,121 @@ using UnityEngine;
 
 public class WorldGraph
 {
-    private List<(WorldNode, int)> edges;
+    private List<List<(WorldNode, ushort)>> edges;
     private List<WorldNode> nodes;
 
-    public IReadOnlyCollection<(WorldNode, int)> Edges { get { return edges.AsReadOnly(); } }
+    public IReadOnlyCollection<List<(WorldNode, ushort)>> Edges { get { return edges.AsReadOnly(); } }
     public IReadOnlyCollection<WorldNode> Nodes { get { return nodes.AsReadOnly(); } }
 
     public WorldGraph() 
     {
-        edges = new List<(WorldNode, int)>();
+        edges = new List<List<(WorldNode, ushort)>>();
         nodes = new List<WorldNode>();
     }
 
-    public void addEdge(WorldNode n1, WorldNode n2, int weight) 
+    private int getIndexFromLocation(string location)
     {
-        edges.Insert(n1.id, (n2, weight));
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            if (nodes[i].location == location)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public void addEdge(WorldNode n1, WorldNode n2, ushort weight) 
+    {
+        if (!nodes.Contains(n1))
+            addNode(n1);
+        if (!nodes.Contains(n2))
+            addNode(n2);
+
+        edges[nodes.IndexOf(n1)].Add((n2, weight));
+    }
+
+    public void addEdge(string location1, string location2, ushort weight)
+    {
+        int n1Index = getIndexFromLocation(location1);
+        int n2Index = getIndexFromLocation(location2);
+
+        if (n1Index == -1)
+        {
+            addNode(location1);
+            n1Index = nodes.Count - 1;
+        }
+        if (n2Index == -1)
+        {
+            addNode(location2);
+            n1Index = nodes.Count - 1;
+        }
+
+        edges[n1Index].Add((nodes[n2Index], weight));
     }
 
     public void addNode(string locationName) 
     {
         WorldNode n = new WorldNode(locationName);
-        if(nodes.Contains(n))
-            nodes.Insert(n.id, n);
-        else nodes.Add(n);
+        addNode(n);
     }
     public void addNode(WorldNode n) 
     {
-        nodes.Insert(n.id, n);
+        if (!nodes.Contains(n)) 
+        {
+            nodes.Add(n);
+            edges.Add(new List<(WorldNode, ushort)>());
+        }
+    }
+
+    public void deleteEdge(WorldNode n1, WorldNode n2)
+    {
+        if(nodes.Contains(n1) && nodes.Contains(n2))
+        {
+            foreach (var edge in edges[nodes.IndexOf(n1)])
+            {
+                if (edge.Item1 == n2)
+                    edges[nodes.IndexOf(n1)].Remove(edge);
+            }
+        } 
+    }
+
+    public void deleteEdge(string location1, string location2)
+    {
+        int n1Index = getIndexFromLocation(location1);
+        int n2Index = getIndexFromLocation(location2);
+
+        if (n1Index != -1 && n1Index != -1)
+        {
+            foreach (var edge in edges[n1Index])
+            {
+                if (edge.Item1 == nodes[n2Index])
+                    edges[n1Index].Remove(edge);
+            }
+        }
+    }
+
+    public void deleteNode(WorldNode n)
+    {
+        if (nodes.Contains(n))
+        {
+            nodes.Remove(n);
+            edges.RemoveAt(nodes.IndexOf(n));
+
+            for(int i = 0; i < edges.Count; i++)
+            {
+                deleteEdge(nodes[i], n);
+            }
+        }
+    }
+    public void deleteNode(string locationName)
+    {
+        int index = getIndexFromLocation(locationName);
+        if(index != -1)
+        {
+            deleteNode(nodes[index]);
+        }
+            
     }
 }
--- Assets/Scripts/DataStructures/WorldNode.cs
+++ Assets/Scripts/DataStructures/WorldNode.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 
 public class WorldNode
 {
-    private static int counter = 0;
     public string location;
-    public int id { get; private set; }
     public PartySheet party;
 
     public WorldNode(string location)
     {
         this.location = location;
         this.party = null;
-        this.id = System.Threading.Interlocked.Increment(ref counter);
     }
-
-    /*~WorldNode()
-    {
-        System.Threading.Interlocked.Decrement(ref counter);
-    }*/
 }
