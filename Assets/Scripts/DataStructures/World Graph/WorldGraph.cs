8dcf8ba9f48b92cd7aac52fac20b4f933aa1d80d
Joshua <jaugees@gmail.com>
2022-03-09 21:22:06 -0800

    graph can be directed or undirected

--- Assets/Scripts/DataStructures/World Graph/WorldGraph.cs
+++ Assets/Scripts/DataStructures/World Graph/WorldGraph.cs
@@ -8,15 +8,17 @@ public class WorldGraph
     private List<List<Edge>> edges;
     private List<WorldNode> nodes;
     private PriorityQueue<(List<WorldNode>, uint)> paths;
+    private bool isDirected;
 
     public IReadOnlyCollection<List<Edge>> Edges { get { return edges.AsReadOnly(); } }
     public IReadOnlyCollection<WorldNode> Nodes { get { return nodes.AsReadOnly(); } }
 
-    public WorldGraph() 
+    public WorldGraph(bool isDirected = true) 
     {
         edges = new List<List<Edge>>();
         nodes = new List<WorldNode>();
         paths = new PriorityQueue<(List<WorldNode>, uint)>();
+        this.isDirected = isDirected;
     }
 
     private Edge getEdge(WorldNode source, WorldNode dest)
@@ -40,6 +42,8 @@ public class WorldGraph
 
     public (List<WorldNode>, float) getShortestPath(WorldNode s, WorldNode d, float DC)
     {
+        //copy edges into a temp data structure then remove any edges that the
+        //party cannot traverse
         List<List<Edge>> tempEdges = new List<List<Edge>>(edges);
         for(int i = 0; i < tempEdges.Count; i++)
         {
@@ -47,40 +51,38 @@ public class WorldGraph
             {
                 if(tempEdges[i][j].difficulty > DC)
                 {
-                    //Debug.Log(String.Format("Removed edge from {0} to {1}", tempEdges[i][j].source.location, tempEdges[i][j].dest.location));
                     tempEdges[i].Remove(tempEdges[i][j]);
                     j--;
-                }
-                    
+                }      
             }
         }
 
+        //initialize stuff for Dijkstra SSSP
         s.d = 0;
         PriorityQueue<WorldNode> queue = new PriorityQueue<WorldNode>();
         foreach(var node in nodes)
-        {
             queue.Insert(node, node.d);
-        }
 
+        //Dijkstra hates UCSC
         while (!queue.IsEmpty())
         {
             WorldNode u = queue.ExtractMin();
             foreach(var edge in tempEdges[nodes.IndexOf(u)])
-            {
                 Relax(edge);
-            }
         }
         
+        //return null if there is no path to d
         if (d.pred == null)
             return (null, -1);
 
+        //otherwise get the path from s to d
         List<WorldNode> path = new List<WorldNode>();
         for (WorldNode i = d; i != s && i != null; i = i.pred)
-        {
             path.Insert(0, i);
-        }
+
         path.Insert(0, s);
 
+        //get total time taken for journey
         float totalTime = 0f;
         for(int i = 0; i < path.Count - 1; i++)
         {
@@ -109,7 +111,8 @@ public class WorldGraph
             addNode(n2);
 
         edges[nodes.IndexOf(n1)].Add(new Edge(n1, n2, timeToTravel, difficulty));
-        // edges[nodes.IndexOf(n1)].Add(new Edge(n2, n1, timeToTravel, difficulty));
+        if(!isDirected)
+            edges[nodes.IndexOf(n2)].Add(new Edge(n2, n1, timeToTravel, difficulty));
     }
 
     public void addEdge(string location1, string location2, float timeToTravel, uint difficulty)
@@ -129,7 +132,8 @@ public class WorldGraph
         }
 
         edges[n1Index].Add(new Edge(nodes[n1Index], nodes[n2Index], timeToTravel, difficulty));
-        //edges.Add(new Edge(nodes[n2Index], nodes[n1Index], timeToTravel, difficulty));
+        if (!isDirected)
+            edges[n2Index].Add(new Edge(nodes[n2Index], nodes[n1Index], timeToTravel, difficulty));
     }
 
     public void addNode(string locationName) 
@@ -153,13 +157,23 @@ public class WorldGraph
             for(int i = 0; i < edges[nodes.IndexOf(n1)].Count; i++)
             {
                 if (edges[nodes.IndexOf(n1)][i].dest == n2)
+                {
                     edges.Remove(edges[i]);
+                    i--;
+                }
+                    
             }
-            //for (int i = 0; i < edges[nodes.IndexOf(n2)].Count; i++)
-            //{
-            //    if (edges[nodes.IndexOf(n2)][i].dest == n1)
-            //        edges.Remove(edges[i]);
-            //}
+            if (!isDirected)
+            {
+                for (int i = 0; i < edges[nodes.IndexOf(n2)].Count; i++)
+                {
+                    if (edges[nodes.IndexOf(n2)][i].dest == n1)
+                    {
+                        edges.Remove(edges[i]);
+                        i--;
+                    }
+                }
+            }
         } 
     }
 
