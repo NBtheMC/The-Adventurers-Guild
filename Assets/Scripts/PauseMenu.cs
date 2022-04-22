a35fd3058714194ff85d6e452dc8da108c2c5bac
parmpreet001 <parmpreet1337@gmail.com>
2022-04-22 15:42:51 -0700

    Updated PauseMenu.cs
    Event is triggered when slider value is changed. Initial slider value set to volume

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,17 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
 
     private TimeSystem timeSystem;
     private CanvasGroup mainCanvasGroup;
+    private Canvas pauseCanvas;
+    private Slider volume1;
+
+    public event EventHandler<float> volume1Change;
     public static bool gamePaused { get; private set; }
     private void Awake()
     {
         timeSystem = GameObject.Find("TimeSystem").GetComponent<TimeSystem>();
         mainCanvasGroup = GameObject.Find("QuestDisplayManager").GetComponent<CanvasGroup>();
+        pauseCanvas = transform.Find("PauseCanvas").GetComponent<Canvas>();
+        volume1 = pauseCanvas.transform.Find("Setting1").Find("Slider").GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        volume1.onValueChanged.AddListener(delegate { volume1Change(this, volume1.value); });
+        volume1.value = GameObject.Find("SoundManager").GetComponent<SoundManagerScript>().GetAudioSrcVolume();
     }
 
     private void Update()
@@ -25,6 +38,7 @@ public class PauseMenu : MonoBehaviour
                 timeSystem.StartTimer();
             gamePaused = !timeSystem.timerActive;
             mainCanvasGroup.interactable = !gamePaused;
+            pauseCanvas.gameObject.SetActive(gamePaused);
         }
     }
 }
