e58ab9e2bd4bbd0bb3da0eee07139d1d3a28352c
parmpreet001 <parmpreet1337@gmail.com>
2022-04-24 20:51:18 -0700

    CharacterTileController uses buttons instead of the weird pointer shit

--- Assets/Scripts/UI/CharacterSelection/CharacterTileController.cs
+++ Assets/Scripts/UI/CharacterSelection/CharacterTileController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CharacterTileController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class CharacterTileController : MonoBehaviour
 {
     [HideInInspector] public CharacterSheet characterSheet; //reference to associated CharacterSheet
     private CharacterBookManager CharBook;
@@ -21,52 +21,36 @@ public class CharacterTileController : MonoBehaviour, IPointerDownHandler, IPoin
         CharInfoUIPrefab = Resources.Load<GameObject>("CharacterInfoUI");
     }
 
-    public void OnPointerDown(PointerEventData pointerEventData)
+    public void CharacterClicked()
     {
         if (PauseMenu.gamePaused)
             return;
-        pointerEventData.useDragThreshold = false;
-        clickPos = Input.mousePosition;
-    }
-
-    public void OnPointerUp(PointerEventData pointerEventData)
-    {
-        if (PauseMenu.gamePaused)
-            return;
-        float deltaX = Input.mousePosition.x - clickPos.x;
-        float deltaY = Input.mousePosition.y - clickPos.y;
-
-        if ((Mathf.Abs(deltaX) < movementDelta && Mathf.Abs(deltaY) < movementDelta))
+        if (!isDisplayed)
         {
-            if (!isDisplayed)
+            if (CharInfoSpawn.transform.childCount != 0)
             {
-
-
-                if (CharInfoSpawn.transform.childCount != 0)
-                {
-                    CharInfoSpawn.transform.GetChild(0).GetComponent<CharacterInfoUI>().DestroyUI();
-                }
-
-                //CharBook.DisplayCharacter(characterSheet);
-                GameObject CharInfoUIObject = Instantiate(CharInfoUIPrefab, CharInfoSpawn.transform);
-                CharInfoUIObject.transform.parent.SetAsLastSibling();
-                CharacterInfoUI characterInfoUI = CharInfoUIObject.GetComponent<CharacterInfoUI>();
-                characterInfoUI.charObject = this.gameObject;
-                characterInfoUI.SetupCharacterInfoUI(characterSheet);
-
-                //Add character portrait
-                if (characterSheet.portrait != null)
-                {
-                    //CharInfoUIObject.AddComponent<Image>().sprite = character.portrait;
-                    CharInfoUIObject.transform.Find("Portrait").GetComponent<Image>().sprite = characterSheet.portrait;
-                }
-
-                isDisplayed = true;
+                CharInfoSpawn.transform.GetChild(0).GetComponent<CharacterInfoUI>().DestroyUI();
             }
-            else 
+
+            //CharBook.DisplayCharacter(characterSheet);
+            GameObject CharInfoUIObject = Instantiate(CharInfoUIPrefab, CharInfoSpawn.transform);
+            CharInfoUIObject.transform.parent.SetAsLastSibling();
+            CharacterInfoUI characterInfoUI = CharInfoUIObject.GetComponent<CharacterInfoUI>();
+            characterInfoUI.charObject = this.gameObject;
+            characterInfoUI.SetupCharacterInfoUI(characterSheet);
+
+            //Add character portrait
+            if (characterSheet.portrait != null)
             {
-                CharInfoSpawn.transform.SetAsLastSibling();
+                //CharInfoUIObject.AddComponent<Image>().sprite = character.portrait;
+                CharInfoUIObject.transform.Find("Portrait").GetComponent<Image>().sprite = characterSheet.portrait;
             }
+
+            isDisplayed = true;
+        }
+        else
+        {
+            CharInfoSpawn.transform.SetAsLastSibling();
         }
     }
 }
