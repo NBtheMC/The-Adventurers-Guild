105a9b9a9af5d6f4f342c03f376856668cc31529
Joshua Augenstein <jaugees@gmail.com>
2022-05-05 16:54:54 -0700

    work started on quest log

--- Assets/Scripts/UI/Quest/QuestDisplayManager.cs
+++ Assets/Scripts/UI/Quest/QuestDisplayManager.cs
@@ -1,24 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class QuestDisplayManager : MonoBehaviour
 {
+    public TimeSystem timeSystem;
     private GameObject questBannerPrefab;
     private GameObject questListContent;
+    private QuestingManager questingManager;
+    private Text pageNumberText;
+    public int pageNumber;
+    public bool currentQuestsDisplayed;
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("QuestingManager").GetComponent<QuestingManager>().QuestAdded += GenerateQuestDisplayUI;
-        //questListContent = GameObject.Find("QuestDisplayManager/QuestDisplay/QuestList/QuestListViewport/ListContent");
+        pageNumber = 0;
+        currentQuestsDisplayed = true;
+        questingManager = GameObject.Find("QuestingManager").GetComponent<QuestingManager>();
+        questingManager.QuestAdded += AddNewQuest;
+        timeSystem = GameObject.Find("TimeSystem").GetComponent<TimeSystem>();
         questListContent = GameObject.Find("QuestDisplayManager/QuestDisplay/QuestList/QuestListViewport/ListContent");
         questBannerPrefab = Resources.Load<GameObject>("QuestBanner");
+        pageNumberText = transform.Find("QuestDisplay/QuestList/PageNumber/Text").gameObject.GetComponent<Text>();
     }
 
-    public void GenerateQuestDisplayUI(object source, QuestSheet quest)
+    public void AddNewQuest(object o, QuestSheet quest)
     {
-        Debug.Log("Making Quest Banner");
+        if (currentQuestsDisplayed)
+            GenerateQuestDisplayUI(quest);
+    }
+
+    private void GenerateQuestDisplayUI(QuestSheet quest)
+    {
         GameObject newQuest = Instantiate(questBannerPrefab);
         //add questBanner obj to questListContent
         newQuest.transform.SetParent(questListContent.transform, false);
@@ -26,7 +41,68 @@ public class QuestDisplayManager : MonoBehaviour
         newQuest.GetComponent<QuestBanner>().questSheet = quest;
         //set banner text
         newQuest.transform.GetChild(0).gameObject.GetComponent<Text>().text = quest.questName;
+    }
 
-        Debug.Log("Done making Quest Banner");
+    private void ClearQuestList()
+    {
+        foreach(Transform child in questListContent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void DisplayQuests() 
+    {
+        ClearQuestList();
+        List<QuestSheet> questsToDisplay;
+
+        if (currentQuestsDisplayed)
+        {
+            questsToDisplay = questingManager.activeQuests.Concat(questingManager.bankedQuests).ToList();
+        }
+        else
+            questsToDisplay = questingManager.finishedQuests[pageNumber];
+
+        foreach (QuestSheet quest in questsToDisplay)
+        {
+            GenerateQuestDisplayUI(quest);
+        }
+    }
+
+    public void PrevPage() 
+    {
+        int currentDay = timeSystem.getTime().day;
+        //if not showing current day
+        if (currentQuestsDisplayed)
+        {
+            currentQuestsDisplayed = false;
+        }
+        else if(pageNumber != 0)
+        {
+            pageNumber--;
+        }
+        DisplayQuests();
+
+        pageNumberText.text = pageNumber + "";
+    }
+
+    public void NextPage()
+    {
+        int currentDay = timeSystem.getTime().day;
+        //if not showing current day
+        if (!currentQuestsDisplayed)
+        {
+            if(pageNumber != currentDay)
+            {
+                pageNumber++;
+            }
+            else
+            {
+                currentQuestsDisplayed = true;
+            }
+            DisplayQuests();
+        }      
+
+        pageNumberText.text = pageNumber + "";
     }
 }
