07385d03e59afa18753cfc29fb8122071a288d57
parmpreet001 <parmpreet1337@gmail.com>
2022-07-10 19:46:49 -0700

    Fixed issue with CreditsSheetManager

--- Assets/Scripts/CreditsSheetManager.cs
+++ Assets/Scripts/CreditsSheetManager.cs
@@ -14,19 +14,12 @@ public class CreditsSheetManager : CharacterSheetManager
         //replace characters
 
         //TODO Commented this out because changes from CharacterSheetManager broke it, fix later
-        /*
-        allAdventurers.Clear();
-        freeAdventurers.Clear();
-        hiredAdventurers.Clear();
-        unhiredAdventurers.Clear();
+
         foreach (CharacterInitialStats character in characters)
         {
             CharacterSheet charSheet = new CharacterSheet(character);
-            allAdventurers.Add(charSheet);
-            freeAdventurers.Add(charSheet);
-            hiredAdventurers.Add(charSheet);
+            adventurerStates.Add(charSheet, AdventurerState.FREE);
             Debug.Log($"Added credits {charSheet.name}");
         }
-        */
     }
 }
