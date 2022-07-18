61571783b2ecd3889ef28068b61f18af6dc7576b
Naman Bhushan <49595001+NBtheMC@users.noreply.github.com>
2022-07-17 19:50:37 -0700

    more stuff

---
+++ Assets/Scripts/UI/CharacterSelection/CreditsTileController.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class CreditsTileController : MonoBehaviour, IPointerDownHandler, IPointerClickHandler
+{
+    [HideInInspector] public CharacterSheet characterSheet; //reference to associated CharacterSheet
+    public ItemDisplayManager displayManager; // The display manager we'll be using.
+    private GameObject CharInfoSpawn;
+    private GameObject CharInfoUIPrefab; // Thre prefab for our character Info.
+    [SerializeField] private float movementDelta = 0; // What is this for?
+    [HideInInspector] public bool isDisplayed = false;
+    bool adventurerBusy = false; // Keeps track of whether the adventurer is busy or not.
+
+    //All the individual bits under this component
+    public Image portrait;
+    public TMPro.TextMeshProUGUI nameText;
+    public TMPro.TextMeshProUGUI combat;
+    public TMPro.TextMeshProUGUI exploration;
+    public TMPro.TextMeshProUGUI negotation;
+    public TMPro.TextMeshProUGUI vitality;
+
+    public void Awake()
+    {
+        displayManager = GameObject.Find("CurrentItemDisplay").GetComponent<ItemDisplayManager>();
+        CharInfoSpawn = displayManager.characterDisplay;
+        CharInfoUIPrefab = Resources.Load<GameObject>("CharacterInfoUI");
+    }
+
+    public void OnPointerDown(PointerEventData pointerEventData)
+    {
+        if (PauseMenu.gamePaused)
+            return;
+        pointerEventData.useDragThreshold = false;
+    }
+
+    public void CharacterClicked()
+    {
+        if (PauseMenu.gamePaused)
+            return;
+
+        //displayManager.DisplayCharacter(characterSheet);
+        //playerInterface.LeftClickedOnCharacter(characterSheet);
+
+        if (!isDisplayed || !displayManager.characterDisplay.activeInHierarchy)
+        {
+            displayManager.DisplayCharacter(true);
+            if (CharInfoSpawn.transform.childCount != 0)
+            {
+                CharInfoSpawn.transform.GetChild(0).GetComponent<CharacterInfoUI>().DestroyUI();
+            }
+
+            GameObject CharInfoUIObject = Instantiate(CharInfoUIPrefab, CharInfoSpawn.transform);
+            CharInfoUIObject.transform.parent.SetAsLastSibling();
+            CharacterInfoUI characterInfoUI = CharInfoUIObject.GetComponent<CharacterInfoUI>();
+            characterInfoUI.charObject = this.gameObject;
+
+            characterInfoUI.SetupCharacterInfoUI(characterSheet);
+
+            //Add character portrait
+            if (characterSheet.portrait != null)
+            {
+                CharInfoUIObject.transform.Find("Portrait").GetComponent<Image>().sprite = characterSheet.portrait;
+            }
+
+            isDisplayed = true;
+        }
+        else if(isDisplayed && displayManager.characterDisplay.activeInHierarchy)
+        {
+            if (CharInfoSpawn.transform.childCount != 0)
+            {
+                CharInfoSpawn.transform.GetChild(0).GetComponent<CharacterInfoUI>().DestroyUI();
+            }
+            displayManager.DisplayCharacter(false);
+            isDisplayed = false;
+        }
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            CharacterClicked();
+        }
+        else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("Right clicked on portrait");
+            //if (!adventurerBusy && displayManager.AssignAdventurer(characterSheet))
+			//{
+                GameObject.Find("PlayerInterface").GetComponent<PlayerInterface>().AdventurerPoolRightClick(characterSheet);
+                //GrayOutPortrait();
+			//}
+
+            /*
+            GameObject questUI = GameObject.Find("QuestDisplayManager/QuestDisplay/CurrentItemDisplay/Quest/QuestUI(Clone)");
+            if (questUI == null || questUI.GetComponent<QuestUI>().questIsActive() || adventurerBusy) return;
+
+            if (questUI.GetComponent<QuestUI>().AssignedCharacters < 4)
+            {
+                GrayOutPortrait();
+                questUI.GetComponent<QuestUI>().AddCharacter(characterSheet);
+            }*/
+        }
+    }
+
+    /// <summary>
+    /// Refreshes this characterTileController to update text and the picture.
+    /// Checks charactersheet stats, picture
+    /// checks what the status is in the CharacterSheetManager to be done soon.
+    /// </summary>
+    public void Refresh()
+	{
+        portrait.sprite = characterSheet.portrait;
+        nameText.text = characterSheet.name;
+        combat.text = characterSheet.getStat(CharacterSheet.StatDescriptors.Combat).ToString();
+        exploration.text = characterSheet.getStat(CharacterSheet.StatDescriptors.Exploration).ToString();
+        negotation.text = characterSheet.getStat(CharacterSheet.StatDescriptors.Negotiation).ToString();
+        vitality.text = characterSheet.getStat(CharacterSheet.StatDescriptors.Constitution).ToString();
+	}
+    
+
+    public void GrayOutPortrait() { portrait.color = new Color32(150, 150, 150, 255); }
+
+    /// <summary>
+    /// Ungrays the thing.
+    /// </summary>
+    public void UngrayPortrait() { portrait.color = new Color32(255, 255, 255, 255); }
+
+    /// <summary>
+    /// Turns the adventurer to full color.
+    /// </summary>
+    public void MarkAdventurerAsFree()
+    {
+        adventurerBusy = false;
+        portrait.color = new Color32(255, 255, 255, 255);
+    }
+
+    /// <summary>
+    /// tints the color black.
+    /// </summary>
+    public void MarkAdventurerAsBusy()
+    {
+        adventurerBusy = true;
+        portrait.color = new Color32(0, 0, 0, 200);
+    }
+}
