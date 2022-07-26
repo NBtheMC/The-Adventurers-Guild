8ce1e8111073a1056c0fa756e92d753e7031db14
parmpreet001 <parmpreet1337@gmail.com>
2022-07-25 17:38:50 -0700

    Partial Merge of branch 'volume-slider' into integration
    There is a conflict regarding scene changes, which will be dealt with in a separate commit

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,17 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
 
     private TimeSystem timeSystem;
     private CanvasGroup mainCanvasGroup;
+    private CanvasGroup pauseCanvasGroup;
     public static bool gamePaused { get; private set; }
     private void Awake()
     {
         timeSystem = GameObject.Find("TimeSystem").GetComponent<TimeSystem>();
         mainCanvasGroup = GameObject.Find("QuestDisplayManager").GetComponent<CanvasGroup>();
+        pauseCanvasGroup = GetComponent<CanvasGroup>();
+        gamePaused = false;
+
+    }
+
+    private void Start()
+    {
+        var canvas = pauseCanvasGroup.transform.Find("Canvas");
+        canvas.transform.Find("MusicSliderContainer").Find("Slider").GetComponent<Slider>().value = GameObject.Find("MusicManager").GetComponent<MusicManager>().GetVolume();
+        canvas.transform.Find("SfxVolumeSlider").Find("Slider").GetComponent<Slider>().value = GameObject.Find("SoundManager").GetComponent<SoundManagerScript>().GetVolume();
     }
 
     private void Update()
@@ -23,26 +35,36 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    public void Pause(){
+    public void Pause() {
+        gamePaused = true;
         timeSystem.StopTimer();
     }
 
-    public void Play(){
+    public void Play() {
+        gamePaused = false;
         timeSystem.StartTimer();
     }
 
+
+
     private void TogglePause()
     {
-        if (timeSystem.timerActive)
-                timeSystem.StopTimer();
-            else
-                timeSystem.StartTimer();
-            gamePaused = !timeSystem.timerActive;
-            mainCanvasGroup.interactable = !gamePaused;
+        gamePaused = !gamePaused;
+        ToggleTimer(!gamePaused);
+        TogglePauseDisplay(gamePaused);
+        mainCanvasGroup.interactable = !gamePaused;
     }
 
-    private void TogglePauseDisplay()
+    private void ToggleTimer(bool active)
     {
+        if (active)
+            timeSystem.StartTimer();
+        else
+            timeSystem.StopTimer();
+    }
 
+    private void TogglePauseDisplay(bool active)
+    {
+        pauseCanvasGroup.transform.Find("Canvas").gameObject.SetActive(active);
     }
 }
