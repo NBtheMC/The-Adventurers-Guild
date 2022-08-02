2d2dec67c85cddce6ad8a490691e63ab9d1284ff
Naman Bhushan <49595001+NBtheMC@users.noreply.github.com>
2022-08-02 15:31:35 -0700

    Level, experience, and required

--- Assets/Scripts/DataStructures/CharacterSheet.cs
+++ Assets/Scripts/DataStructures/CharacterSheet.cs
@@ -7,8 +7,8 @@ public class CharacterSheet
     // The character's name
     public string name { get; private set; }
 
-    // The character's stats
-    private Dictionary<StatDescriptors, int> statlines = new Dictionary<StatDescriptors, int>();
+    // The character's stats and experience
+    private Dictionary<StatDescriptors, int[]> statlines; //current level, current experience, and experience required to level up 
 
     // The characters relationships
     public Adventurer adventurer;
@@ -30,7 +30,11 @@ public class CharacterSheet
     public CharacterSheet(string nameInput, Dictionary<StatDescriptors, int> statslinesInput)
 	{
         name = nameInput;
-        statlines = new Dictionary<StatDescriptors, int>(statslinesInput);
+        statlines = new Dictionary<StatDescriptors, int[]>();
+        foreach( KeyValuePair<StatDescriptors, int> kvp in statslinesInput )
+        {
+            statlines.Add(kvp.Key, new int[] {kvp.Value,0,(int)Mathf.Pow(kvp.Value,2)*200});
+        }
         adventurer = new Adventurer();
         adventurer.characterSheet = this;
     }
@@ -41,9 +45,9 @@ public class CharacterSheet
     public CharacterSheet(CharacterInitialStats characterStats)
     {
         name = characterStats.name;
-        statlines.Add(StatDescriptors.Combat, characterStats.combat);
-        statlines.Add(StatDescriptors.Charisma, characterStats.charisma);
-        statlines.Add(StatDescriptors.Exploration, characterStats.exploration);
+        statlines.Add(StatDescriptors.Combat, new int[] {characterStats.combat, 0,(int)Mathf.Pow(characterStats.combat+1,2)*200});
+        statlines.Add(StatDescriptors.Charisma, new int[] {characterStats.charisma, 0,(int)Mathf.Pow(characterStats.charisma+1,2)*200});
+        statlines.Add(StatDescriptors.Exploration, new int[] {characterStats.exploration, 0,(int)Mathf.Pow(characterStats.exploration+1,2)*200});
         adventurer = new Adventurer();
         adventurer.characterSheet = this;
         portrait = characterStats.portrait;
@@ -58,14 +62,30 @@ public class CharacterSheet
     /// If not, 0.</returns>
     public int getStat(StatDescriptors stat) {
         if (!statlines.ContainsKey(stat)) { return 0; }
-        return statlines[stat];
+        return statlines[stat][0];
     }
 
     // Add the following stat into the character's statlines.
     public void addStat(StatDescriptors statname, int number)
     {
-		if (statlines.ContainsKey(statname)) { statlines[statname] = number; }
-        statlines.Add(statname, number);
+		if (statlines.ContainsKey(statname)) { 
+            statlines[statname][0] = number;
+            statlines[statname][1] = 0; 
+            statlines[statname][2] = (int)(Mathf.Pow(number+1,2)*200); 
+        }
+        statlines.Add(statname, new int[] {number, 0,(int)Mathf.Pow(number+1,2)*200});
+        Debug.Log("Added "+number+" to "+ name);
+    }
+
+    public void addStatExperience(StatDescriptors statname, int number)
+    {
+        statlines[statname][1]+=number;
+        //levelling up
+        while(statlines[statname][1]>=statlines[statname][2]){
+            statlines[statname][0]++;   //level
+            statlines[statname][1] -= statlines[statname][2]; //change to remainder
+            statlines[statname][2] = (int)Mathf.Pow(statlines[statname][0],2)*200;
+        }
     }
 
     /// <summary>
@@ -74,7 +94,11 @@ public class CharacterSheet
     /// <returns>A whole dictionary of the statname and the current stat number.</returns>
     public Dictionary<StatDescriptors,int> GetStatSheet()
 	{
-        return new Dictionary<StatDescriptors, int>(statlines);
+        Dictionary<StatDescriptors, int> justStats = new Dictionary<StatDescriptors, int>();
+        foreach(KeyValuePair<StatDescriptors, int[]> kvp in statlines){
+            justStats.Add(kvp.Key, kvp.Value[0]);
+        }
+        return justStats;
 	}
 
 }
